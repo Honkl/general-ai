b98e080d552ec7a059e6bfeacbb3719d07a2b9fd
Honkl <jan.kluj@gmail.com>
2016-10-23 20:53:45 +0200

    I/O redirected
    Redirected input and output in games to python's standard I/O

--- Controller/script.py
+++ Controller/script.py
@@ -5,13 +5,28 @@ import json
 REQUEST = 'request.json'
 ANSWER = 'answer.txt'
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-f = open(os.path.join(__location__, REQUEST))
-request_data = json.load(f)
-target = open(os.path.join(__location__, ANSWER), 'w')
+np.random.seed(42)
 
-moves = request_data['PossibleMoves']
-index = np.random.randint(len(moves))
+while (True):
+    line = input()
 
-target.write(str(index))
-target.close()
+    if (line == "END"):
+        break
+
+    # In case of using files:
+    #__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    #f = open(os.path.join(__location__, REQUEST))
+    #request_data = json.load(f)
+    #target = open(os.path.join(__location__, ANSWER), 'w')
+
+    request_data = json.loads(line)
+
+    moves = request_data['PossibleMoves']
+    index = np.random.randint(len(moves))
+
+    #target.write(str(index))
+    #target.close()
+
+    print(str(index))
+
+
