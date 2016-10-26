923e5a58eb8d9d8d422d13a844b10ef5bdcc5de3
Honkl <jan.kluj@gmail.com>
2016-10-27 00:50:10 +0200

    AI controller improved + .gitignore

--- Controller/controller.py
+++ Controller/controller.py
@@ -6,6 +6,7 @@ from __future__ import division
 import os
 from subprocess import call
 
+
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 prefix = os.path.join(__location__, "../../")
 
@@ -13,12 +14,30 @@ MARIO = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin;" + prefix + "MarioAI/Ma
 GAME2048 = prefix + "2048/2048/bin/Debug/2048.exe"
 ALHAMBRA = prefix + "general-ai/Game-interfaces/Alhambra/AlhambraInterface/AlhambraInterface/bin/Debug/AlhambraInterface.exe"
 
-#call(MARIO)
-#call(GAME2048)
-call(ALHAMBRA)
+# TODO: Relative path
+TORCS = "cmd.exe \"/C cd \"C:/Program Files (x86)/torcs\" & wtorcs.exe -r "
+TORCS_XML = "\"" + prefix + "general-ai/Game-interfaces/TORCS/race_config.xml\""
+TORCS_TIMEOUT = " & timeout 2 /nobreak"
+TORCS_CLIENT = " & java -cp \"" + prefix + "general-ai/Game-interfaces/TORCS/scr-client/classes\" scr.Client scr.SimpleDriver port:3002\""
 
-#print(MARIO)
-#print(GAME2048)
-#print(ALHAMBRA)
+
+def start_torcs():
+    command = TORCS + TORCS_XML + TORCS_TIMEOUT + TORCS_CLIENT
+    print(command)
+    call(command)
+
+def start_mario():
+    call(MARIO)
+
+def start_2048():
+    call(GAME2048)
+
+def start_alhambra():
+    call(ALHAMBRA)
+
+#start_mario()
+#start_2048()
+#start_alhambra()
+start_torcs()
 
 print("Finished")
