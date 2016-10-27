13c6023272616aaed5b6a3d6d912fb8dc7187252
Honkl <jan.kluj@gmail.com>
2016-10-27 15:56:48 +0200

    Torcs batch file added + controller paths fixed

--- Controller/controller.py
+++ Controller/controller.py
@@ -8,22 +8,20 @@ from subprocess import call
 
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-prefix = os.path.join(__location__, "../../")
+prefix = os.path.join(__location__, "..\\..\\")
 
-MARIO = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin;" + prefix + "MarioAI/MarioAI4J-Playground/bin;" + prefix + "MarioAI/MarioAI4J-Playground/lib/*\" mario.GeneralAgent"
-GAME2048 = prefix + "2048/2048/bin/Debug/2048.exe"
-ALHAMBRA = prefix + "general-ai/Game-interfaces/Alhambra/AlhambraInterface/AlhambraInterface/bin/Debug/AlhambraInterface.exe"
+MARIO = "java -cp \"" + prefix + "MarioAI\\MarioAI4J\\bin;" + prefix + "MarioAI\\MarioAI4J-Playground\\bin;" + prefix + "MarioAI\\MarioAI4J-Playground\\lib\\*\" mario.GeneralAgent"
+GAME2048 = prefix + "2048\\2048\\bin\\Debug\\2048.exe"
+ALHAMBRA = prefix + "general-ai\\Game-interfaces\\Alhambra\\AlhambraInterface\\AlhambraInterface\\bin\\Debug\\AlhambraInterface.exe"
 
 # TODO: Relative path
-TORCS = "cmd.exe \"/C cd \"C:/Program Files (x86)/torcs\" & wtorcs.exe -r "
-TORCS_XML = "\"" + prefix + "general-ai/Game-interfaces/TORCS/race_config.xml\""
-TORCS_TIMEOUT = " & timeout 2 /nobreak"
-TORCS_CLIENT = " & java -cp \"" + prefix + "general-ai/Game-interfaces/TORCS/scr-client/classes\" scr.Client scr.SimpleDriver port:3002\""
+TORCS = "\"" + prefix + "general-ai\\Game-interfaces\\TORCS\\torcs_starter.bat\""
+TORCS_XML = " \"" + prefix + "general-ai\\Game-interfaces\\TORCS\\race_config.xml\""
+TORCS_CLIENT = " \"" + prefix + "general-ai\\Game-interfaces\\TORCS\\scr-client\\classes\""
 
 
 def start_torcs():
-    command = TORCS + TORCS_XML + TORCS_TIMEOUT + TORCS_CLIENT
-    print(command)
+    command = TORCS + TORCS_XML + TORCS_CLIENT
     call(command)
 
 def start_mario():
@@ -35,9 +33,9 @@ def start_2048():
 def start_alhambra():
     call(ALHAMBRA)
 
-#start_mario()
-#start_2048()
-#start_alhambra()
+start_mario()
+start_2048()
+start_alhambra()
 start_torcs()
 
 print("Finished")
---
+++ Game-interfaces/TORCS/torcs_starter.bat
@@ -0,0 +1,8 @@
+:: Batch file to run torcs with some settings
+:: First parameter (%1) is xml config file
+:: Second parameter (%2) is path to java classes with torcs client
+cd "C:/Program Files (x86)/torcs"
+start /b wtorcs.exe -r %1
+::timeout 1 /nobreak
+PING 1.1.1.1 -n 1 -w 1000 > NUL
+java -cp %2 scr.Client scr.SimpleDriver port:3002
