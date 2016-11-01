141d14617b6cb0663b390067d800930437a8bc9c
Honkl <jan.kluj@gmail.com>
2016-11-01 16:57:16 +0100

    Loading config file for each game

--- Controller/script.py
+++ Controller/script.py
@@ -2,10 +2,29 @@ import numpy as np
 import os
 import json
 
-REQUEST = 'request.json'
-ANSWER = 'answer.txt'
-
 np.random.seed(42)
+
+def load_config(config_file):
+    with open (config_file, "r") as file:
+        for line in file.readlines():
+            line_parts = line.strip().split("=")
+            name = line_parts[0]
+            value = line_parts[1]
+            if name == "game_phases":
+                game_phases = int(value)
+            elif name == "input_sizes":
+                input_sizes = list(map(int, value.split(",")))
+            elif name == "output_sizes":
+                output_sizes = list(map(int, value.split(",")))
+    return game_phases, input_sizes, output_sizes
+
+###################################
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+prefix = os.path.dirname(__location__) + "\\Game-interfaces\\"
+game_phases = -1
+input_sizes = -1
+output_sizes = -1
 
 while (True):
     line = input()
@@ -13,9 +32,22 @@ while (True):
     if (line == "END"):
         break
 
+    if not line.startswith("{"):
+        f = prefix + line + "\\" + line + "_config.txt"
+        game_phases, input_sizes, output_sizes = load_config(f)
+        continue
+
+    # Already in game; received game information, next move is expected from AI
     request_data = json.loads(line)
+    curr_phase = int(request_data["current_phase"])
 
-    moves = request_data['possible_moves']
-    index = np.random.randint(len(moves))
+    if (input_sizes[curr_phase] != len(request_data["state"])):
+        raise RuntimeError("Wrong number of inputs")
 
-    print(str(index))
+    result = ""
+    for i in range(output_sizes[curr_phase]):
+        result += str(np.random.random())
+        if (i < output_sizes[curr_phase] - 1):
+            result += " "
+
+    print(result)
--- Game-interfaces/TORCS/torcs_starter.bat
+++ Game-interfaces/TORCS/torcs_starter.bat
@@ -3,6 +3,7 @@
 :: Second parameter (%2) is path to java classes with torcs client
 cd "C:/Program Files (x86)/torcs"
 start /b wtorcs.exe -r %1 -t 1000000 -nofuel -nodamage -nolaptime
+::start /b wtorcs.exe -r ./config/raceman/race_config.xml -t 1000000 -nofuel -nodamage -nolaptime
 ::timeout 2 /nobreak
 PING 1.1.1.1 -n 1 -w 1000 > NUL
 java -cp %2 scr.Client scr.GeneralAIDriver port:3002 directory:%3
