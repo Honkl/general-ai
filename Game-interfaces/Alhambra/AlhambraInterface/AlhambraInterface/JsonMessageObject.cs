ecb4e3ced5e64e4ab64b4a72633d7eaa8c8b0d7d
Jan Klůj <jan.kluj@gmail.com>
2016-11-18 18:23:34 +0100

    Alhambra input for AI rescaled

--- Game-interfaces/Alhambra/AlhambraInterface/AlhambraInterface/JsonMessageObject.cs
+++ Game-interfaces/Alhambra/AlhambraInterface/AlhambraInterface/JsonMessageObject.cs
@@ -22,31 +22,56 @@ namespace AlhambraInterface
             private set;
         }
 
-        private static List<Card> Cards = null;
-        private static List<Building> Buildings = null;
+        private static List<Card> allCards = null;
+        private static List<Building> allBuildings = null;
+        /// <summary>
+        /// Number of card colors.
+        /// </summary>
+        private const int TotalCardTypes = 4;
+        /// <summary>
+        /// Number of building colors.
+        /// </summary>
+        private const int TotalBuildingTypes = 6;
+        /// <summary>
+        /// Number of max value of one card.
+        /// </summary>
+        private const int MaxCardValue = 9;
+        /// <summary>
+        /// Number of max playres that can play the game.
+        /// </summary>
+        private const int MaxNumberOfPlayers = 6;
+        /// <summary>
+        /// Each card is three times in the package.
+        /// </summary>
+        private const int MaxNumberOfOneCardType = 3;
+        /// <summary>
+        /// Max value of building in whole game.
+        /// </summary>
+        private const int MaxBuildingValue = 13;
+
 
         public static void InitStaticValues()
         {
-            if (Cards != null || Buildings != null)
+            if (allCards != null || allBuildings != null)
             {
                 throw new AlhambraException("Reinitializing AlhambraInterface storage.");
             }
 
-            Cards = new List<Card>();
-            Buildings = new List<Building>();
+            allCards = new List<Card>();
+            allBuildings = new List<Building>();
 
             CardType type = CardType.Blue;
             foreach (CardType t in Enum.GetValues(typeof(CardType)))
             {
                 for (int value = Card.MinValue; value <= Card.MaxValue; value++)
                 {
-                    Cards.Add(new Card(type, value));
+                    allCards.Add(new Card(type, value));
                 }
             }
 
             CodeStorage cs = new CodeStorage();
             cs.InitializeStorage();
-            Buildings = cs.GetBuildingList();
+            allBuildings = cs.GetBuildingList();
         }
 
         /// <summary>
@@ -60,37 +85,33 @@ namespace AlhambraInterface
             current_phase = gamePhase;
         }
 
+        /// <summary>
+        /// Encodes the state of the game into array of double. Most of the values are rescaled to interval of [0,1].
+        /// </summary>
+        /// <param name="representedPlayer">Player whose data will be encoded.</param>
+        /// <returns>Double array with all encoded game data of the specified player.</returns>
         private double[] EncodeState(Player representedPlayer)
         {
             List<double> state = new List<double>();
 
             Game game = representedPlayer.game;
-            state.Add(game.NumberOfPlayers);
 
-            int maxNumberOfPlayers = 6;
-            foreach (int p in game.points)
-            {
-                state.Add(p);
-            }
-            for (int i = 0; i < maxNumberOfPlayers - game.points.Length; i++)
-            {
-                state.Add(0);
-            }
+            state.Add((double)game.NumberOfPlayers / (double)MaxNumberOfPlayers);
 
-            foreach (Card c in Cards)
+            foreach (Card c in allCards)
             {
-                state.Add((int)c.Type);
-                state.Add(c.Value);
-                state.Add(GetNumberOfCards(c, representedPlayer.cards));
+                state.Add((double)c.Type / (double)TotalCardTypes);
+                state.Add((double)c.Value / (double)MaxCardValue);
+                state.Add((double)GetNumberOfCards(c, representedPlayer.cards) / (double)MaxNumberOfOneCardType);
             }
 
             for (int i = 0; i < Game.MarketSize; i++)
-			{
+            {
                 if (!game.nonAvailableCardsOnMarket[i])
                 {
                     Card c = game.cardsOnMarket[i];
-                    state.Add((int)c.Type);
-                    state.Add(c.Value);
+                    state.Add((double)c.Type / (double)TotalCardTypes);
+                    state.Add((double)c.Value / (double)MaxCardValue);
                 }
                 else
                 {
@@ -99,12 +120,10 @@ namespace AlhambraInterface
                 }
             }
 
-            // TODO: Karty ostatních hráčů (počty)???
-
-            foreach (Building b in Buildings)
+            foreach (Building b in allBuildings)
             {
-                state.Add((int)b.Type);
-                state.Add(b.Value);
+                state.Add((double)b.Type / (double)TotalBuildingTypes);
+                state.Add((double)b.Value / (double)MaxBuildingValue);
                 state.Add(b.IsInStorage ? 1 : 0);
                 foreach (bool value in b.Walls)
                 {
@@ -113,12 +132,18 @@ namespace AlhambraInterface
 
                 state.Add((representedPlayer.constructed.Contains(b) || representedPlayer.postponed.Contains(b)) ? 1 : 0);
 
-                if (representedPlayer.constructed.Contains(b))
+                bool found = false;
+                foreach (var constr in representedPlayer.constructed)
                 {
-                    state.Add(b.Position.Row);
-                    state.Add(b.Position.Column);
+                    if (constr.Equals(b))
+                    {
+                        state.Add((double)constr.Position.Row / (double)Game.MapSize);
+                        state.Add((double)constr.Position.Column / (double)Game.MapSize);
+                        found = true;
+                        break;
+                    }
                 }
-                else
+                if (!found)
                 {
                     state.Add(0);
                     state.Add(0);
@@ -132,8 +157,8 @@ namespace AlhambraInterface
                     state.AddRange(new double[] { 0, 0, 0, 0, 0, 0 });
                     continue;
                 }
-                state.Add((int)b.Type);
-                state.Add(b.Value);
+                state.Add((double)b.Type / (double)TotalBuildingTypes);
+                state.Add((double)b.Value / (double)MaxBuildingValue);
                 foreach (bool value in b.Walls)
                 {
                     state.Add(value ? 1 : 0);
@@ -147,7 +172,6 @@ namespace AlhambraInterface
         /// </summary>
         /// <param name="c">Card to count.</param>
         /// <param name="cards">List where to count.</param>
-        /// <returns></returns>
         private int GetNumberOfCards(Card card, List<Card> cards)
         {
             int result = 0;
