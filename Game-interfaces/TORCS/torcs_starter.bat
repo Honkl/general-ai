d637e563dec2bed20f1f47b5ca59c7d04ab00f75
Honkl <jan.kluj@gmail.com>
2016-11-30 00:20:28 +0100

    Torcs server output redirected

--- Game-interfaces/TORCS/torcs_starter.bat
+++ Game-interfaces/TORCS/torcs_starter.bat
@@ -8,7 +8,7 @@
 :: %7 is a full path of model configuration file
 
 cd %5
-start /b wtorcs.exe -r %1 -t 1000000 -nofuel -nodamage -nolaptime
+start /b wtorcs.exe -r %1 -t 1000000 -nofuel -nodamage -nolaptime > NUL
 ::start /b wtorcs.exe -r ./config/raceman/race_config.xml -t 1000000 -nofuel -nodamage -nolaptime
 ::timeout 2 /nobreak
 PING 1.1.1.1 -n 1 -w 500 > NUL
