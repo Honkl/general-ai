0f6e3b31b51b62baa241307da02056fa534d5cb6
Jan Klůj <jan.kluj@gmail.com>
2016-12-02 19:30:55 +0100

    Game batch size + mario output format changed

--- Controller/games/mario.py
+++ Controller/games/mario.py
@@ -5,23 +5,18 @@ import re
 
 
 class Mario(Game):
-    def __init__(self, model_config_file):
+    def __init__(self, model_config_file, game_batch_size):
         self.model_config_file = model_config_file
+        self.game_batch_size = game_batch_size
 
     def run(self):
-        command = MARIO + PYTHON_SCRIPT + PYTHON_EXE + " \"" + self.model_config_file + "\""
+        command = MARIO + PYTHON_SCRIPT + PYTHON_EXE + " \"" + self.model_config_file + "\" " + str(
+            self.game_batch_size)
+
         p = subprocess.Popen(command, stdout=subprocess.PIPE)
         result = p.communicate()[0].decode('ascii')
         result = re.split("\\r\\n|\\n", result)
 
-        scores = []
         for line in result:
-            if line.startswith("status"):
-                for item in line.split(";"):
-                    name, value = item.partition("=")[::2]
-                    scores.append((name, value))
-                    if (name == "passedDistance"):
-                        print(value)
-                        return float(value)
-                break
-        return scores
+            if "passed_distance" in line:
+                return float(line.split("=")[1].strip())
