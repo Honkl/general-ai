5607cb74e96419bd2e140c5366788a43fbd58b13
Jan Klůj <jan.kluj@gmail.com>
2017-02-11 20:18:41 +0100

    Delimiter added

--- Controller/constants.py
+++ Controller/constants.py
@@ -1,11 +1,14 @@
 import os
+import platform
 
 loc = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 # prefix = Master directory
 prefix = os.path.dirname(os.path.dirname(loc)) + "/"  # cut two last directories
 
-MARIO = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin;" + prefix + "MarioAI/MarioAI4J-Playground/bin;" + prefix + "MarioAI/MarioAI4J-Playground/lib/*\" mario.GeneralAgent"
-MARIO_VISUALISATION = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin;" + prefix + "MarioAI/MarioAI4J-Playground/bin;" + prefix + "MarioAI/MarioAI4J-Playground/lib/*\" mario.VisualizationTool"
+delimiter = ";" if (platform.system() == "Windows") else ":"
+
+MARIO = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin" + delimiter + prefix + "MarioAI/MarioAI4J-Playground/bin" + delimiter + prefix + "MarioAI/MarioAI4J-Playground/lib/*\" mario.GeneralAgent"
+MARIO_VISUALISATION = "java -cp \"" + prefix + "MarioAI/MarioAI4J/bin" + delimiter + prefix + "MarioAI/MarioAI4J-Playground/bin" + delimiter + prefix + "MarioAI/MarioAI4J-Playground/lib/*\" mario.VisualizationTool"
 
 GAME2048 = prefix + "2048/2048/bin/Release/2048.exe"
 GAME2048_ADVANCED_TOOL = prefix + "2048/Visualization/bin/Release/Visualization.exe"
