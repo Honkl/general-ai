37706ab4fe823bbc83bc92543235aee63effa615
Jan Klůj <jan.kluj@gmail.com>
2017-03-02 20:01:29 +0100

    torcs classes added to repository
    - portability reasons, do not need build on different machine

--- Game-interfaces/TORCS/scr-client/classes/TORCS_manual_run.bat
+++
@@ -1,5 +0,0 @@
-:: Run from current directory. TORCS server has to run before this batch file.
-:: 1. parameter is a model file to run
-
-set mypath=%cd%
-java -cp ".;../lib/*" scr.Client scr.GeneralAIDriver ai_script:%mypath%"/../../../../Controller/script.py" python:"C:\\Program Files\\Anaconda3\\envs\\tensorflow-gpu\\python.exe" model_config:%1
